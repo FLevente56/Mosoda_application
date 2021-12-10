89876c7a37241bdcc4718070cb9785d089de13dd
Bernat99 <64952501+Bernat99@users.noreply.github.com>
2021-12-10 19:53:25 +0200

    adatbazis-app kapcsolat

--- app/src/main/java/com/example/mosoda_app/DBHelper.kt
+++ app/src/main/java/com/example/mosoda_app/DBHelper.kt
@@ -5,70 +5,57 @@ import android.content.Context
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 
-class DBHelper(context: Context, factory: SQLiteDatabase.CursorFactory?) :
-    SQLiteOpenHelper(context, DATABASE_NAME, factory, DATABASE_VERSION) {
+class DBHelper(val context: Context) {
+    companion object {
 
+        //general
+        val dbVersion = 1
+        val dbName = "mosodapp.db"
 
-
-    // below is the method for creating a database by a sqlite query
-    override fun onCreate(db: SQLiteDatabase) {
-        // below is a sqlite query, where column names
-        // along with their data types is given
-        val query = ("CREATE TABLE " + TABLE_NAME + " ("
-                + ID_COL + " INTEGER PRIMARY KEY, " +
-                NAME_COl + " TEXT," +
-                ADDR + " TEXT," + TEL_NUM + " TEXT)")
-
-        // we are calling sqlite
-        // method for executing our query
-        db.execSQL(query)
+        //table and columns
+        //val maincat_tbl = "maincat"
+        //val _id = "id"
+        //val _filter = "filter"
     }
 
-    override fun onUpgrade(db: SQLiteDatabase, p1: Int, p2: Int) {
-        // this method is to check if table already exists
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME)
-        onCreate(db)
+    val database:SQLiteDatabase
+
+    init {
+        database = open()
     }
 
-    // This method is for adding data in our database
-    fun addMember(name : String, addr : String, tel_num : String ){
-
-        val values = ContentValues()
-
-        // we are inserting our values
-        // in the form of key-value pair
-        values.put(NAME_COl, name)
-        values.put(ADDR, addr)
-        values.put(TEL_NUM, tel_num)
-
-        // here we are creating a
-        // writable variable of
-        // our database as we want to
-        // insert value in our database
-        val db = this.writableDatabase
-
-        // all values are inserted into database
-        db.insert(TABLE_NAME, null, values)
-
-        // at last we are
-        // closing our database
-        db.close()
+    private fun open(): SQLiteDatabase {
+        val dbFile = context.getDatabasePath("$dbName.db")
+        if (!dbFile.exists()){
+            try {
+                val checkDB = context.openOrCreateDatabase("$dbName.db", Context.MODE_PRIVATE,null)
+                checkDB.close()
+                copyDatabase(dbFile)
+            }catch (e: IOException){
+                throw RuntimeException("Error opening db")
+            }
+        }
+        return SQLiteDatabase.openDatabase(dbFile.path, null, SQLiteDatabase.OPEN_READWRITE)
     }
 
-    companion object{
-        private val DATABASE_NAME = "MOSODAPP"
+    private fun copyDatabase(dbFile: File) {
+        val iss = context.assets.open("$dbName.db")
+        val os = FileOutputStream(dbFile)
 
-        private val DATABASE_VERSION = 1
+        val buffer = ByteArray(1024)
+        while (iss.read(buffer) > 0) {
+            os.write(buffer)
+        }
+        os.flush()
+        os.close()
+        iss.close()
+    }
 
-        val TABLE_NAME = "Szemelyek"
-
-        val ID_COL = "ID"
-
-        val NAME_COl = "nev"
-
-        val ADDR = "cim"
-
-        val TEL_NUM = "telefonszam"
+    fun close() {
+        database.close()
     }
 }
