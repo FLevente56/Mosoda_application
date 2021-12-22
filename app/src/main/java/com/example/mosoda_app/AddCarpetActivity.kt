7b989050915981c8ef26fe00853b8e96a293c8c3
Bernat99 <64952501+Bernat99@users.noreply.github.com>
2021-12-22 14:52:30 +0200

    create add carpet layout

---
+++ app/src/main/java/com/example/mosoda_app/AddCarpetActivity.kt
@@ -0,0 +1,11 @@
+package com.example.mosoda_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddCarpetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_carpet)
+    }
+}
--- app/src/main/java/com/example/mosoda_app/HomePage.kt
+++ app/src/main/java/com/example/mosoda_app/HomePage.kt
@@ -1,13 +1,20 @@
 package com.example.mosoda_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class HomePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
 
+        var add_carpet_button = findViewById<Button>(R.id.add_carpet_button)
 
+        add_carpet_button.setOnClickListener{
+            val intent = Intent(this, AddCarpetActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
